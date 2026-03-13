d2ebab7b0280bc909d2cb87fd05e24a2f82f2c30
Dilip Vishwakarma <2106dilip@gmail.com>
2026-03-14 03:33:50 +0530

    refactor: Clean up imports and simplify Toaster position logic in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { Toaster } from 'react-hot-toast'
-import { store } from './redux/store.js';
+import { Toaster } from "react-hot-toast";
+import { store } from "./redux/store.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
-    {
-      screen.width < 640 ? (
-        <Toaster position='top-right'/>
-      ): (
-        <Toaster position='top-center' />
-      )
-    }
+    <Toaster position="top-center" />
   </Provider>,
-)
+);
